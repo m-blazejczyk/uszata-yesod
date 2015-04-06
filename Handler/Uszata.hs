5705c1c92d1ee2af434950866ebd09e7a2f626b3
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2015-04-05 22:36:26 -0400

    Remaining 'poradnik' pages

--- Handler/Uszata.hs
+++ Handler/Uszata.hs
@@ -19,3 +19,33 @@ getMieszkanieR = do
   defaultLayout $ do
     setTitle "Jak przygotować mieszkanie? - Uszata strona"
     $(widgetFile "poradnik-mieszkanie")
+
+getSkadWziacR :: Handler Html
+getSkadWziacR = do
+  defaultLayout $ do
+    setTitle "Skad wziąć królika? - Uszata strona"
+    $(widgetFile "poradnik-skad-wziac")
+
+getPrzynoszenieR :: Handler Html
+getPrzynoszenieR = do
+  defaultLayout $ do
+    setTitle "Przynoszenie do domu - Uszata strona"
+    $(widgetFile "poradnik-przynoszenie")
+
+getDlaDzieckaR :: Handler Html
+getDlaDzieckaR = do
+  defaultLayout $ do
+    setTitle "Królik dla dziecka? - Uszata strona"
+    $(widgetFile "poradnik-dla-dziecka")
+
+getPlusyMinusyR :: Handler Html
+getPlusyMinusyR = do
+  defaultLayout $ do
+    setTitle "Plusy i minusy - Uszata strona"
+    $(widgetFile "poradnik-plusy-minusy")
+
+getPodstawoweR :: Handler Html
+getPodstawoweR = do
+  defaultLayout $ do
+    setTitle "Podstawowe informacje - Uszata strona"
+    $(widgetFile "poradnik-podstawowe-info")
