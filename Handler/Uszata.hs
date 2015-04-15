bfe2cbaf00221e8e3ab218e22c2569e1198781a6
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2015-04-14 22:05:46 -0400

    Minor change - a commit test

--- Handler/Uszata.hs
+++ Handler/Uszata.hs
@@ -17,35 +17,35 @@ getPoradnikR = do
 getMieszkanieR :: Handler Html
 getMieszkanieR = do
   contentLayout "poradnik" $ do
-    setTitle "Jak przygotować mieszkanie? - Uszata strona"
+    setTitle "Jak przygotować mieszkanie? - Poradnik użytkownika - Uszata strona"
     $(widgetFile "poradnik-mieszkanie")
 
 getSkadWziacR :: Handler Html
 getSkadWziacR = do
   contentLayout "poradnik" $ do
-    setTitle "Skąd wziąć królika? - Uszata strona"
+    setTitle "Skąd wziąć królika? - Poradnik użytkownika - Uszata strona"
     $(widgetFile "poradnik-skad-wziac")
 
 getPrzynoszenieR :: Handler Html
 getPrzynoszenieR = do
   contentLayout "poradnik" $ do
-    setTitle "Przynoszenie do domu - Uszata strona"
+    setTitle "Przynoszenie do domu - Poradnik użytkownika - Uszata strona"
     $(widgetFile "poradnik-przynoszenie")
 
 getDlaDzieckaR :: Handler Html
 getDlaDzieckaR = do
   contentLayout "poradnik" $ do
-    setTitle "Królik dla dziecka? - Uszata strona"
+    setTitle "Królik dla dziecka? - Poradnik użytkownika - Uszata strona"
     $(widgetFile "poradnik-dla-dziecka")
 
 getPlusyMinusyR :: Handler Html
 getPlusyMinusyR = do
   contentLayout "poradnik" $ do
-    setTitle "Plusy i minusy - Uszata strona"
+    setTitle "Plusy i minusy - Poradnik użytkownika - Uszata strona"
     $(widgetFile "poradnik-plusy-minusy")
 
 getPodstawoweR :: Handler Html
 getPodstawoweR = do
   contentLayout "poradnik" $ do
-    setTitle "Podstawowe informacje - Uszata strona"
+    setTitle "Podstawowe informacje - Poradnik użytkownika - Uszata strona"
     $(widgetFile "poradnik-podstawowe-info")
